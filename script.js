2db510e3d346df05f1b153a0dd1acb9bf9a98b53
nelioyann <nanayannick1998@gmail.com>
2022-06-16 02:00:05 +0200

    feat: Breaking changes

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
-
 const APP = {
   code: null,
   input: null,
   elPreview: document.querySelector(".preview"),
   elCode: document.querySelector(".code"),
-  elInput:document.querySelector(".generator_input"),
-  elForm:document.querySelector(".generator"),
-  elGenerated:document.querySelector(".generated"),
-  elCopier:document.querySelector(".copier"),
+  elInput: document.querySelector(".generator_input"),
+  elForm: document.querySelector(".generator"),
+  elGenerated: document.querySelector(".generated"),
+  elCopier: document.querySelector(".copier"),
+  elPreviewer: document.querySelector(".previewer"),
+  elDialog: document.querySelector("#dialog"),
   init() {
-    // let urn = ""
-    APP.elForm.addEventListener("submit", APP.getInput)
-    APP.elCopier.addEventListener("click", APP.copyCode)
+    APP.elForm.addEventListener("submit", APP.getInput);
+    APP.elCopier.addEventListener("click", APP.copyCode);
+    APP.elPreviewer.addEventListener("click", APP.showPreview);
     // const link = `<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:activity:6757299314709561344"  frameborder="0" allowfullscreen="" title="Post intégré"></iframe>`
-    
   },
-  getCode (postLink){
+  getCode(postLink) {
     const regex = /(\d+)/gm;
     let m;
     let frame;
@@ -27,32 +26,45 @@ const APP = {
       }
       // The result can be accessed through the `m`-variable.
       m.forEach((match, groupIndex) => {
-        console.log(`Found match, group ${groupIndex}: ${match}`);
-        if(match.length > 3){
-          
-          frame = 
-  `<iframe 
-      src="https://www.linkedin.com/embed/feed/update/urn:li:activity:${match}" 
-      frameborder="0" title="LinkedIn">
-</iframe>`;
+        // console.log(`Found match, group ${groupIndex}: ${match}`);
+        if (match.length > 3) {
+          frame = `<span style="color: #008;">&lt;iframe</span> 
+<span style="color: #606;">src=</span><span style="color: #080;">"https://www.linkedin.com/embed/feed/update/urn:li:activity:${match}"</span>
+<span style="color: #008;">&gt;&lt;/iframe&gt;</span>`;
+          code = `<iframe 
+          src="https://www.linkedin.com/embed/feed/update/urn:li:activity:${match}" 
+          frameborder="0" title="LinkedIn" scrolling="no">
+          </iframe>`;
         }
       });
     }
-    return(frame)
+    return {frame, code};
   },
-  getInput(e){
+  getInput(e) {
     e.preventDefault();
-    plausible("Generated");
-    let test = "https://www.linkedin.com/posts/association-prisme_local-accompagnement-associationprisme-activity-6775784238177390592-tGEg/";
-    APP.code = APP.getCode(APP.elInput.value)
-    APP.elPreview.innerHTML = APP.code
-    APP.elCode.innerText = APP.code
-    APP.elGenerated.classList.remove("hidden")
-    console.log(APP.code)
+    APP.elDialog.showModal();
+    try {
+      plausible("Generated");
+    } catch (e) {
+      console.log(e);
+    }
+    console.log((window.ADS_BLOCKED = true));
+    setTimeout(() => {
+      APP.code = APP.getCode(APP.elInput.value).frame;
+      APP.elPreview.innerHTML = APP.getCode(APP.elInput.value).code;
+      APP.elCode.innerHTML = APP.code;
+      // document.querySelector("[data-revealable]").dataset.revealable = "visible:true";
+    }, 100);
+    // APP.elGenerated.classList.remove("hidden");
+    // console.log(APP.code);
   },
-  copyCode(){
+  copyCode() {
     navigator.clipboard.writeText(APP.code);
-    
+  },
+  showPreview(){
+    console.log("Display preview");
+      document.querySelector(".preview_wrapper").dataset.revealable = "visible:true";
+
   }
 };
 document.addEventListener("DOMContentLoaded", APP.init);
