01b578ce91ba763e13e51c19cd237864787a36fe
nelioyann <nanayannick1998@gmail.com>
2022-06-20 01:10:18 +0200

    chore - cleaning up

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const APP = {
   code: null,
+  prettyCode: null,
   input: null,
   elPreview: document.querySelector(".preview"),
   elCode: document.querySelector(".code"),
@@ -13,32 +14,29 @@ const APP = {
     APP.elForm.addEventListener("submit", APP.getInput);
     APP.elCopier.addEventListener("click", APP.copyCode);
     APP.elPreviewer.addEventListener("click", APP.showPreview);
+    dialogPolyfill.registerDialog(APP.elDialog);
     // const link = `<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:activity:6757299314709561344"  frameborder="0" allowfullscreen="" title="Post intégré"></iframe>`
   },
   getCode(postLink) {
     const regex = /(\d+)/gm;
     let m;
-    let frame;
     while ((m = regex.exec(postLink)) !== null) {
-      // This is necessary to avoid infinite loops with zero-width matches
       if (m.index === regex.lastIndex) {
         regex.lastIndex++;
       }
-      // The result can be accessed through the `m`-variable.
-      m.forEach((match, groupIndex) => {
-        // console.log(`Found match, group ${groupIndex}: ${match}`);
+      m.forEach((match) => {
         if (match.length > 3) {
-          frame = `<span style="color: #008;">&lt;iframe</span> 
+          prettyCode = `<span style="color: #008;">&lt;iframe</span> 
 <span style="color: #606;">src=</span><span style="color: #080;">"https://www.linkedin.com/embed/feed/update/urn:li:activity:${match}"</span>
 <span style="color: #008;">&gt;&lt;/iframe&gt;</span>`;
           code = `<iframe 
           src="https://www.linkedin.com/embed/feed/update/urn:li:activity:${match}" 
-          frameborder="0" title="LinkedIn" scrolling="no">
+          frameborder="0" title="LinkedIn">
           </iframe>`;
         }
       });
     }
-    return {frame, code};
+    return {prettyCode, code};
   },
   getInput(e) {
     e.preventDefault();
@@ -48,23 +46,22 @@ const APP = {
     } catch (e) {
       console.log(e);
     }
-    console.log((window.ADS_BLOCKED = true));
     setTimeout(() => {
-      APP.code = APP.getCode(APP.elInput.value).frame;
-      APP.elPreview.innerHTML = APP.getCode(APP.elInput.value).code;
-      APP.elCode.innerHTML = APP.code;
-      // document.querySelector("[data-revealable]").dataset.revealable = "visible:true";
+      APP.code = APP.getCode(APP.elInput.value).code;
+      APP.prettyCode = APP.getCode(APP.elInput.value).prettyCode;
+      APP.elPreview.innerHTML = APP.code;
+      APP.elCode.innerHTML = APP.prettyCode;
     }, 100);
-    // APP.elGenerated.classList.remove("hidden");
-    // console.log(APP.code);
+
   },
   copyCode() {
-    navigator.clipboard.writeText(APP.code);
+    navigator.clipboard.writeText(APP.code).then(res => {
+      alert("Embed code successfully copied");
+    }).catch(err => alert("Something went wrong, copy the code manually"));
   },
   showPreview(){
     console.log("Display preview");
       document.querySelector(".preview_wrapper").dataset.revealable = "visible:true";
-
   }
 };
 document.addEventListener("DOMContentLoaded", APP.init);
